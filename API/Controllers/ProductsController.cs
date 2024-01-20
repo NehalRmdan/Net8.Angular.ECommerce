e2dd1c253112cd844dc3195b9b1bb5b6e44dacd9
Nehal Ramadan <Nehal.Ramadan@linkdev.com>
2024-01-20 15:12:45 +0200

    Section2-API-Basics

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            var products= await _storeContext.Products.ToListAsync();
+            if(products is null)
+                return NotFound("products are not exist.");
+            return Ok(products);
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+             var product= await _storeContext.Products.FindAsync(id);
+            if(product is null)
+                return NotFound($"product {id} is not exist.");
+            return Ok(product);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
